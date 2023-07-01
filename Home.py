6e23d78743d9f59f145a8d3f04118a2193d68189
Antonia-Lake <10202150412@stu.ecnu.edu.cn>
2023-07-01 12:36:59 +0800

    try to solve GeoPandas import problem

--- Home.py
+++ Home.py
@@ -1,3 +1,11 @@
+import conda
+import os
+
+conda_file_dir = conda.__file__
+conda_dir = conda_file_dir.split('lib')[0]
+proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
+os.environ["PROJ_LIB"] = proj_lib
+
 import streamlit as st
 import pandas as pd
 import geopandas as gpd
