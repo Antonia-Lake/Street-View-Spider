9a7470836e9c1c34a545629e387589f65100446f
Antonia-Lake <10202150412@stu.ecnu.edu.cn>
2023-07-02 16:26:58 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,24 +1,3 @@
-import os
-
-# def default_proj_lib():
-#     proj_lib = os.getenv('PROJ_LIB')
-#     if proj_lib not in (None, 'PROJ_LIB'):
-#         return proj_lib
-#     try:
-#         import conda
-#     except ImportError:
-#         conda = None
-#     if conda is not None or os.getenv('CONDA_PREFIX') is None:
-#         conda_file_dir = conda.__file__
-#         conda_dir = conda_file_dir.split('lib')[0]
-#         proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
-#         if os.path.exists(proj_lib):
-#             return proj_lib
-#         return None
-#     return None
-
-# default_proj_lib()
-
 import streamlit as st
 import pandas as pd
 import geopandas as gpd
@@ -28,10 +7,11 @@ text = """
 # :house_with_garden: 这是一个百度街景数据采集工具
 ## 作者信息
 姓名：来陈璐\\
-学号：10202150412\\
 班级：21级地理科学拔尖班
 ## 项目简介
-本项目是一个百度街景数据采集工具，可以通过输入一个点数据文件，自动采集该点附近的街景数据，并将采集结果保存为一个csv文件。
+- 本项目是一个百度街景数据采集工具。用户可以上传一个点数据文件（**CSV/GeoJSON**），自动采集该点附近四个方向（东南西北）的街景数据。
+- 采集数据后，会使用语义分割模型对街景图像进行分割，得到街景中的语义内容，并计算绿视率和天空率。
+- 最后，用户可以下载采集的街景数据、语义分割结果、绿视率和天空率。
 
 ## 环境依赖
 - Anaconda
@@ -47,5 +27,3 @@ text = """
 """
 
 st.markdown(text)
-
-
